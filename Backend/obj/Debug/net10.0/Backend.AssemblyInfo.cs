b10a3f9bd6e0ebc3fa780299f7d292fd6c79eb70
Kondacs Pál <33621043+kpali89@users.noreply.github.com>
2025-12-11 19:10:28 +0000

    v.0.02_API_Done

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c75aa7f95e39cf816320c175b88b344fd6e505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b8f4248db38a28a1b674f6c901748f8d4641401")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
